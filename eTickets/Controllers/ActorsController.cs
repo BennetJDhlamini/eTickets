52cccade0871244eb9f1a58eda71b3505c0e264f
bennetjdhlamini@gmail.com <bennetjdhlamini@gmail.com>
2021-12-30 16:57:33 +0200

    Added delete function

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -73,21 +73,21 @@ namespace eTickets.Controllers
         }
 
         //Get: Actors/Delete/1
-        //public async Task<IActionResult> Delete(int id)
-      //  {
-      //      var actorDetails = await _service.GetByIdAsync(id);
-      //      if (actorDetails == null) return View("NotFound");
-      //      return View(actorDetails);
-     //   }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
 
-      //  [HttpPost, ActionName("Delete")]
-      //  public async Task<IActionResult> DeleteConfirmed(int id)
-      //  {
-      //      var actorDetails = await _service.GetB>yIdAsync(id);
-      //      if (actorDetails == null) return View("NotFound");
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
 
-      //      await _service.DeleteAsync(id);
-      //      return RedirectToAction(nameof(Index));
-      //  }
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -22,9 +22,11 @@ namespace eTickets.Data.Services
             await _context.SaveChangesAsync();
         }
 
-        public void Delete(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var results = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            _context.Actors.Remove(results);
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<Actor>> GetAllAsync()
