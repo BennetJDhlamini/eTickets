a1c717edd3681870c4c097a4e6a0c3724d1af47d
bennetjdhlamini@gmail.com <bennetjdhlamini@gmail.com>
2021-12-30 16:41:38 +0200

    Added edit view

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -54,23 +54,23 @@ namespace eTickets.Controllers
         }
 
         //Get: Actors/Edit/1
-       // public async Task<IActionResult> Edit(int id)
-        //{
-        //    var actorDetails = await _service.GetByIdAsync(id);
-        //    if (actorDetails == null) return View("NotFound");
-        //    return View(actorDetails);
-        //}
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
 
-       // [HttpPost]
-       // public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
-      //  {
-       //     if (!ModelState.IsValid)
-       //     {
-       //         return View(actor);
-      //      }
-       //     await _service.UpdateAsync(id, actor);
-       //     return RedirectToAction(nameof(Index));
-      //  }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+        }
 
         //Get: Actors/Delete/1
         //public async Task<IActionResult> Delete(int id)
