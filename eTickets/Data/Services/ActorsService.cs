520181b6f13d115a90d2ddc169773f85ef03f655
bennetjdhlamini@gmail.com <bennetjdhlamini@gmail.com>
2021-12-30 19:46:55 +0200

    ActorsService configuration with EntityBaseRepository

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -1,4 +1,5 @@
-﻿using eTickets.Models;
+﻿using eTickets.Data.Base;
+using eTickets.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -7,45 +8,8 @@ using System.Threading.Tasks;
 
 namespace eTickets.Data.Services
 {
-    public class ActorsService : IActorsService
+    public class ActorsService : EntityBaseRepository<Actor>, IActorsService
     {
-        private readonly AppDbContext _context;
-
-        public ActorsService(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task AddAsync(Actor actor)
-        {
-            await _context.Actors.AddAsync(actor);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            var results = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            _context.Actors.Remove(results);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<IEnumerable<Actor>> GetAllAsync()
-        {
-            var results = await _context.Actors.ToListAsync();
-            return results;
-        }
-
-        public async Task<Actor> GetByIdAsync(int id)
-        {
-            var results = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
-            return results;
-        }
-
-        public async Task<Actor> UpdateAsync(int id, Actor newActor)
-        {
-            _context.Update(newActor);
-            await _context.SaveChangesAsync();
-            return newActor;
-        }
+        public ActorsService(AppDbContext context) : base(context) { }
     }
 }
