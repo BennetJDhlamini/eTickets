4bbc3cdc83a73c319ddee52f4e42bb01ad9daf1e
bennetjdhlamini@gmail.com <bennetjdhlamini@gmail.com>
2021-12-31 07:31:30 +0200

    Added ShoppingCartItem model

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -36,6 +36,6 @@ namespace eTickets.Data
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
-        //public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
